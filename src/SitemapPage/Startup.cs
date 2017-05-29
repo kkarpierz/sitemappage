8bd4380f5cec28e25ec81cfe317fe19dd5c1c951
Karol <Karol@KAROL>
2017-05-29 21:47:25 +0200

    Initial commit. form to enter url, view changes

---
+++ src/SitemapPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SitemapPage.Startup))]
+namespace SitemapPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
